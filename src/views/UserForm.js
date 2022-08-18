50ec06856e3cc2034d14a55b28ce3028f82833d9
Eliardo <57414074+Eliard0@users.noreply.github.com>
2022-08-18 16:17:21 -0300

    crud concluido, blibiotecas ultilizadas contextApi, React Hooks (reduce), reactNative elements

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,59 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import UsersContext from '../context/UsersContext'
 
-export default props => {
+export default ({ route, navigation }) => {
+    const [user, setUser] = useState(route.params ? route.params : {})
+
+    const { dispatch } = useContext(UsersContext)
+
     return (
-        <Text>ola mundo 1 </Text>
+        <View style={style.form}>
+            <Text>Nome</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={name => setUser({ ...user, name })}
+                placeholder="Informe o Nome"
+                value={user.name}
+            />
+            <Text>E-mail</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={email => setUser({ ...user, email })}
+                placeholder="Informe o E-mail"
+                value={user.email}
+            />
+            <Text>Url do Avatar</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={avatarUrl => setUser({ ...user, avatarUrl })}
+                placeholder="Informe a url"
+                value={user.avatarUrl}
+            />
+            <Button
+                title='Salvar'
+                onPress={() => {
+                    dispatch({
+                        type: user.id ? 'updateUser' : 'createUser',
+                        payload: user,
+                    })
+                    navigation.goBack()
+                }}
+            />
+        </View>
     )
- }
+}
+
+const style = StyleSheet.create({
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 5,
+        marginBottom: 10,
+        padding: 5
+    },
+    form: {
+        padding: 12,
+    }
+})
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,20 +1,45 @@
-import { ListItem, Avatar } from '@rneui/base'
-import React from 'react'
-import { View, FlatList } from 'react-native'
-import users from '../mock/data'
-// import { ListItem } from 'react-native-elements'
+import React, { useContext } from 'react'
+import { ListItem, Avatar, Icon } from '@rneui/base'
+import { View, FlatList, Alert } from 'react-native'
+import UsersContext from '../context/UsersContext'
 
 export default props => {
 
+    const { state, dispatch } = useContext(UsersContext)
+
     function getUserItem({ item: user }) {
         return (
-            <ListItem key={user.id}>
+            <ListItem key={user.id} bottomDivider>
+                <ListItem.Chevron onPress={() => props.navigation.navigate("UserForm", user)} />
                 <Avatar source={{ uri: user.avatarUrl }} />
                 <ListItem.Content>
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
                 </ListItem.Content>
-                <ListItem.Chevron onPress={() => props.navigation.navigate("UserForm")}></ListItem.Chevron>
+                <Icon
+                    name='edit'
+                    color='#00aced'
+                    onPress={() => props.navigation.navigate("UserForm", user)}
+                />
+                <Icon
+                    name='delete'
+                    color='red'
+                    onPress={
+                        () => Alert.alert('Excluir', 'deseja realmente excluir esse usuario', [
+                            {
+                                text: 'Sim',
+                                onPress() {
+                                   dispatch({
+                                    type: 'deleteUser',
+                                    payload: user,
+                                   })
+                                }
+                            },
+                            {
+                                text: 'Não'
+                            }
+                        ])}
+                />
             </ListItem>
         )
     }
@@ -23,9 +48,24 @@ export default props => {
         <View>
             <FlatList
                 keyExtractor={user => user.id.toString()}
-                data={users}
+                data={state.users}
                 renderItem={getUserItem}
             />
         </View>
     )
 }
+
+// função delete não esta sendo usada porque não consigo ter acesso ao id
+    // function confirmDelete(users) {
+    //     Alert.alert('Excluir', 'deseja realmente excluir esse usuario',[
+    //         {
+    //             text: 'Sim',
+    //             onPress(){
+    //                 console.warn(users.id)
+    //             }
+    //         },
+    //         {
+    //             text: 'Não'
+    //         }
+    //     ])
+    // }
